0f5d92d03fedc4f7dc36d9ba807d1dde14114e4a
nari1021 <y.shhh000@gmail.com>
2022-01-15 18:00:16 +0900

    add: all of Entities

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,36 +1,39 @@
-import { ApiProperty } from "@nestjs/swagger";
 import {
     Column,
     CreateDateColumn,
+    UpdateDateColumn,
     DeleteDateColumn,
     Entity,
     PrimaryGeneratedColumn,
+    ManyToOne,
+    OneToMany,
 } from 'typeorm';
+import { ApiProperty } from "@nestjs/swagger";
+import { Area } from "src/area/entities/area.entity";
+import { Campus } from "src/campus/entities/campus.entity";
+import { Board } from 'src/board/entities/board.entity';
+import { Comment } from "src/comment/entities/comment.entity"
+import { Report } from "src/report/entities/report.entity"
 
-@Entity('user')
+@Entity('users')
 export class User {
     @PrimaryGeneratedColumn('increment')
     @ApiProperty({ description: '사용자의 id' })
     id: number;
 
-    @Column({
-        nullable: true
-    })
+    @Column()
     @ApiProperty({ description: '사용자가 로그인한 SNS 타입' })
-    social: string;
+    social!: string;
 
     @Column({
-        unique: true,
-        nullable: true
+        unique: true
     })
     @ApiProperty({ description: '사용자가 로그인한 SNS의 ID 값' })
-    social_id: string;
+    social_id!: string;
 
-    @Column({
-        nullable: true
-    })
+    @Column()
     @ApiProperty({ description: '사용자 이름' })
-    name: string;
+    name!: string;
 
     @Column({
         nullable: true,
@@ -38,25 +41,38 @@ export class User {
     @ApiProperty({ description: '사용자 연락처' })
     phone: string;
 
-    @Column({
-        nullable: true
-    })
+    @Column()
     @ApiProperty({ description: '사용자 생일' })
-    birthday: Date;
+    birthday!: Date;
 
-    @Column({
-        nullable: true
-    })
+    @Column()
     @ApiProperty({ description: '사용자 호칭' })
-    position: string;
+    position!: string;
+
+    /* Relations */
+
+    @OneToMany(() => Board, board => board.user)
+    boardList: Board[];
+
+    @OneToMany(() => Report, report => report.user)
+    reportList: Report[];
+
+    @OneToMany(() => Comment, comment => comment.user)
+    commentList: Comment[];
+
+    @ManyToOne(() => Area, area => area.userList, { onDelete: 'CASCADE' })
+    area!: Area;
+
+    @ManyToOne(() => Campus, campus => campus.userList, { onDelete: 'CASCADE' })
+    campus!: Campus;
 
     /* Date Columns */
 
     @CreateDateColumn()
-    createdAt: Date;
+    createdAt!: Date;
 
     @UpdateDateColumn()
-    updatedAt: Date;
+    updatedAt!: Date;
 
     @DeleteDateColumn()
     deletedAt: Date | null;
