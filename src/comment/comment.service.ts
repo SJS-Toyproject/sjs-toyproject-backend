733dbf8be1745e21ccba22c6159b995b257f79aa
nari1021 <y.shhh000@gmail.com>
2022-01-30 16:00:27 +0900

    update: comment service structure

--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -1,4 +1,7 @@
+import { NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
+import { Post } from "src/board/entities/board.entity";
+import { User } from "src/user/entities/user.entity";
 import { Repository } from "typeorm";
 import { CreateCommentDto } from "./dto/create-comment.dto";
 import { UpdateCommentDto } from "./dto/update-comment.dto";
@@ -7,31 +10,45 @@ import { Comment } from "./entities/comment.entity";
 export class CommentService {
     constructor(
         @InjectRepository(Comment)
-        private readonly commentRespository: Repository<Comment>
+        private readonly CommentRepository: Repository<Comment>,
+        @InjectRepository(Post)
+        private readonly PostRepository: Repository<Post>,
+        @InjectRepository(User)
+        private readonly UserRepository: Repository<User>,
     ) { }
 
-    async create(createCommentDto: CreateCommentDto): Promise<Comment> {
-        const createComment = await this.commentRespository.save({
-            content: createCommentDto.content
-        });
+    async create(commentData: CreateCommentDto): Promise<Comment> {
+        const { postId, userId, content } = commentData;
+        const post = await this.PostRepository.findOne(postId);
+        if (post == null) {
+            throw new NotFoundException();
+        }
 
-        return createComment;
+        const user = await this.PostRepository.findOne(userId);
+        if (user === undefined) {
+            throw new NotFoundException();
+        }
+
+        const comment = this.CommentRepository.create({ post, user, content });
+        return await this.CommentRepository.save(comment);
     }
 
     async update({ id, content }: UpdateCommentDto) {
-        const newComment = await this.commentRespository.findOne({ id: id })
+        const newComment = await this.CommentRepository.findOne({ id: id })
         newComment.content = content;
 
-        const updateComment = await this.commentRespository.save(newComment);
+        const updateComment = await this.CommentRepository.save(newComment);
         return updateComment;
     }
 
-    async findCommentById(id: number) {
-        const commentList = await this.commentRespository.find({ boardId: id });
+    async findCommentById(id: number): Promise<Comment[]> {
+        const post = await this.PostRepository.findOne({ postId });
+        const commentList = await this.CommentRepository.find({ post: post });
         return commentList;
     }
 
-    async remove(id: number) {
-        await this.commentRespository.delete(id);
+    async remove(id: number): Promise<void> {
+        await this.CommentRepository.delete(id);
+        return;
     }
 }
--- src/comment/dto/create-comment.dto.ts
+++ src/comment/dto/create-comment.dto.ts
@@ -1,9 +1,15 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty, IsString } from "class-validator";
+import { IsNotEmpty, IsNumber, IsString } from "class-validator";
 
 export class CreateCommentDto {
+    @IsNumber()
+    @IsNotEmpty()
+    postId: number;
+
+    @IsNumber()
+    @IsNotEmpty()
+    userId: number;
+
     @IsString()
     @IsNotEmpty()
-    @ApiProperty({ example: '댓글 내용을 작성합니다.', description: '댓글 내용' })
     content: string;
 }
